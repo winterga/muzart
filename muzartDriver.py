b77255f94b346a6505f3e93301534fb03d3fd536
Bryce Keating <bryce@Bryces-MacBook-Pro.local>
2023-07-27 16:34:19 -0500

    updated muzartDriver with screen information

--- muzartDriver.py
+++ muzartDriver.py
@@ -1,2 +1,9 @@
 import noteFinder
+import turtle
 
+#code for initializing screen turtle object
+screen = turtle.Screen()
+screen.bgcolor('black')
+screen.setup(1920,1080)
+screen.tracer(0)
+screen.colormode(255)
