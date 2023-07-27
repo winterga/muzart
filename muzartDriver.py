71b7bddf1d559d29b00c2fb4f8591cdeab06a967
winterga <greyson.a.wintergerst@vanderbilt.edu>
2023-07-27 18:04:19 -0500

    did some work on driver

--- muzartDriver.py
+++ muzartDriver.py
@@ -1,4 +1,5 @@
-import noteFinder
+import noteFinder as nf
+import objectOrientedShapes as shapes
 import turtle
 
 #code for initializing screen turtle object
@@ -7,3 +8,10 @@ screen.bgcolor('black')
 screen.setup(1920,1080)
 screen.tracer(0)
 screen.colormode(255)
+
+muzart = nf.Muzart(file='/vanderbiltCS/SyBBURE/SU23/more-complicated-demo-song-2-both-hands.mp3')
+noteList = muzart.run()
+
+
+
+
